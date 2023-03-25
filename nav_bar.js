e0d718b2ff31e0dccc3a000f7cad3d11717f9262
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-03-25 16:30:20 +0530

    version 3.0

--- nav_bar.js
+++ nav_bar.js
@@ -1,11 +1,11 @@
 var nav_bar=document.querySelector("#nav-logo a");
 var expanded=false;
-var arr=document.querySelectorAll("#nav-container a");
+var nav_list=document.querySelectorAll("#nav-container a");
 var nav_container=document.querySelector("#nav-container");
 
 nav_bar.addEventListener('click',toggle);
 for(let i=0;i<arr.length;i++){
-    arr[i].addEventListener('click',toggle);
+    nav_list[i].addEventListener('click',toggle);
 }
 
 function toggle(){
@@ -22,5 +22,57 @@ function toggle(){
         expanded=true;
         nav_container.style.height="300px";
         // console.log("Expand");
+    }
+}
+
+
+//.............logic for scroll bar auto fill.............................
+
+
+
+var skill_container=document.querySelector("#skill-container");
+var arr=document.querySelectorAll("#skill-container .skill-percentage");
+var isFilled=new Array(arr.length);
+
+// var isFilled=false;
+
+window.addEventListener('scroll',animateToAll);
+
+function animateToAll(){
+    for(let i=0;i<arr.length;i++){
+        animate(arr[i]);
+    }
+}
+
+function fill(eleObject){
+    // console.log("fill called");
+    // for(let i=0;arr.length;i++){
+    //     let d=arr[i].getAttribute("data-percentage");
+    //     arr[i].style.width=d;
+    // }
+    // console.log(eleObject);
+    let d=eleObject.getAttribute("data-percentage");
+    eleObject.style.width=d;
+}
+
+function empty(eleObject){
+    // console.log("empty called");
+    // for(let i=0;arr.length;i++){
+    //     arr[i].style.width=0+"%";
+    // }
+    // console.log(eleObject);
+    eleObject.style.width=0+"%";
+}
+
+function animate(eleObject){
+    let yPos=eleObject.getBoundingClientRect().top;
+    let index=parseInt(eleObject.getAttribute("data-index"));
+    if(!isFilled[index]&&window.innerHeight>yPos){
+        isFilled[index]=true;
+        fill(eleObject);
+    }
+    else if(isFilled[index]&&window.innerHeight<yPos){
+        isFilled[index]=false;
+        empty(eleObject);
     }
 }
