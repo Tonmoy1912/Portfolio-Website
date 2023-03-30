51065761c41593c4cec2b1cb94b39baa39e1fe3a
Tonmoy Biswas <tonmoybiswas19122002@gamil.com>
2023-03-30 20:52:44 +0530

    IIFE in JS

--- nav_bar&skill_auto_fill.js
+++ nav_bar&skill_auto_fill.js
@@ -1,81 +1,83 @@
-var nav_bar=document.querySelector("#nav-logo a");
-var expanded=false;
-var nav_list=document.querySelectorAll("#nav-container a");
-var nav_container=document.querySelector("#nav-container");
+(function (){
+    var nav_bar=document.querySelector("#nav-logo a");
+    var expanded=false;
+    var nav_list=document.querySelectorAll("#nav-container a");
+    var nav_container=document.querySelector("#nav-container");
 
-nav_bar.addEventListener('click',toggle);
-for(let i=0;i<arr.length;i++){
-    nav_list[i].addEventListener('click',toggle);
-}
-
-function toggle(){
-    if(window.innerWidth>700){
-        // console.log("inner width > 700")
-        return ;
+    nav_bar.addEventListener('click',toggle);
+    for(let i=0;i<nav_list.length;i++){
+        nav_list[i].addEventListener('click',toggle);
     }
-    if(expanded){
-        expanded=false;
-        nav_container.style.height="0px";
-        // console.log("shrink");
+
+    function toggle(){
+        if(window.innerWidth>700){
+            // console.log("inner width > 700")
+            return ;
+        }
+        if(expanded){
+            expanded=false;
+            nav_container.style.height="0px";
+            // console.log("shrink");
+        }
+        else{
+            expanded=true;
+            nav_container.style.height="300px";
+            // console.log("Expand");
+        }
     }
-    else{
-        expanded=true;
-        nav_container.style.height="300px";
-        // console.log("Expand");
+
+
+    //.............logic for scroll bar auto fill.............................
+
+
+
+    var skill_container=document.querySelector("#skill-container");
+    var arr=document.querySelectorAll("#skill-container .skill-percentage");
+    var isFilled=new Array(arr.length);
+    for(let i=0;i<isFilled.length;i++){
+        isFilled[i]=true;
     }
-}
 
+    // var isFilled=false;
 
-//.............logic for scroll bar auto fill.............................
+    window.addEventListener('scroll',animateToAll);
 
-
-
-var skill_container=document.querySelector("#skill-container");
-var arr=document.querySelectorAll("#skill-container .skill-percentage");
-var isFilled=new Array(arr.length);
-for(let i=0;i<isFilled.length;i++){
-    isFilled[i]=true;
-}
-
-// var isFilled=false;
-
-window.addEventListener('scroll',animateToAll);
-
-function animateToAll(){
-    for(let i=0;i<arr.length;i++){
-        animate(arr[i]);
+    function animateToAll(){
+        for(let i=0;i<arr.length;i++){
+            animate(arr[i]);
+        }
     }
-}
 
-function fill(eleObject){
-    // console.log("fill called");
-    // for(let i=0;arr.length;i++){
-    //     let d=arr[i].getAttribute("data-percentage");
-    //     arr[i].style.width=d;
-    // }
-    // console.log(eleObject);
-    let d=eleObject.getAttribute("data-percentage");
-    eleObject.style.width=d;
-}
-
-function empty(eleObject){
-    // console.log("empty called");
-    // for(let i=0;arr.length;i++){
-    //     arr[i].style.width=0+"%";
-    // }
-    // console.log(eleObject);
-    eleObject.style.width=0+"%";
-}
-
-function animate(eleObject){
-    let yPos=eleObject.getBoundingClientRect().top;
-    let index=parseInt(eleObject.getAttribute("data-index"));
-    if(!isFilled[index]&&window.innerHeight>yPos){
-        isFilled[index]=true;
-        fill(eleObject);
+    function fill(eleObject){
+        // console.log("fill called");
+        // for(let i=0;arr.length;i++){
+        //     let d=arr[i].getAttribute("data-percentage");
+        //     arr[i].style.width=d;
+        // }
+        // console.log(eleObject);
+        let d=eleObject.getAttribute("data-percentage");
+        eleObject.style.width=d;
     }
-    else if(isFilled[index]&&window.innerHeight<yPos){
-        isFilled[index]=false;
-        empty(eleObject);
+
+    function empty(eleObject){
+        // console.log("empty called");
+        // for(let i=0;arr.length;i++){
+        //     arr[i].style.width=0+"%";
+        // }
+        // console.log(eleObject);
+        eleObject.style.width=0+"%";
     }
-}
+
+    function animate(eleObject){
+        let yPos=eleObject.getBoundingClientRect().top;
+        let index=parseInt(eleObject.getAttribute("data-index"));
+        if(!isFilled[index]&&window.innerHeight>yPos){
+            isFilled[index]=true;
+            fill(eleObject);
+        }
+        else if(isFilled[index]&&window.innerHeight<yPos){
+            isFilled[index]=false;
+            empty(eleObject);
+        }
+    }
+})();
--- smooth_scroll.js
+++ smooth_scroll.js
@@ -1,62 +1,64 @@
-var arr=document.querySelectorAll(".nav-button a");
-var prevEventId=new Array();
-var count=0;
-var endCall=0;
+(function (){
+    var arr=document.querySelectorAll(".nav-button a");
+    var prevEventId=new Array();
+    var count=0;
+    var endCall=0;
 
-function bringTo(event){
-    if(prevEventId.length==1){
-        clearInterval(prevEventId.pop());
-    }
-    event.preventDefault();
-    let sectionId=this.getAttribute("href");
-    let section=document.querySelector(sectionId);
-    let pos=section.getBoundingClientRect();
-    let yIndex=pos.top;
-    var IdForSmoothScroll;
-    if(yIndex==0){
-        return ;
-    }
-    if(yIndex>0){
-        IdForSmoothScroll=setInterval(goDown,10);
-    }
-    else{
-        IdForSmoothScroll=setInterval(goUp,10);
-    }
-    prevEventId.push(IdForSmoothScroll);
-    function goDown(){
-        pos=section.getBoundingClientRect();
-        yIndex=pos.top;
-        if(yIndex<=0){
-            clearInterval( IdForSmoothScroll);
-            prevEventId.pop();
-            // endCall++;
-            // console.log("endCall= "+endCall);
+    function bringTo(event){
+        if(prevEventId.length==1){
+            clearInterval(prevEventId.pop());
+        }
+        event.preventDefault();
+        let sectionId=this.getAttribute("href");
+        let section=document.querySelector(sectionId);
+        let pos=section.getBoundingClientRect();
+        let yIndex=pos.top;
+        var IdForSmoothScroll;
+        if(yIndex==0){
             return ;
         }
-        window.scrollBy(0,20);
-        // count++;
-        // console.log("Scroll call = "+count);
-    }
-
-    function goUp(){
-        pos=section.getBoundingClientRect();
-        yIndex=pos.top;
-        if(yIndex>=0){
-            clearInterval( IdForSmoothScroll);
-            prevEventId.pop();
-            // endCall++;
-            // console.log("endCall= "+endCall);
-            return ;
+        if(yIndex>0){
+            IdForSmoothScroll=setInterval(goDown,10);
         }
-        window.scrollBy(0,-20);
-        // count++;
-        // console.log("Scroll call = "+count);
+        else{
+            IdForSmoothScroll=setInterval(goUp,10);
+        }
+        prevEventId.push(IdForSmoothScroll);
+        function goDown(){
+            pos=section.getBoundingClientRect();
+            yIndex=pos.top;
+            if(yIndex<=0){
+                clearInterval( IdForSmoothScroll);
+                prevEventId.pop();
+                // endCall++;
+                // console.log("endCall= "+endCall);
+                return ;
+            }
+            window.scrollBy(0,20);
+            // count++;
+            // console.log("Scroll call = "+count);
+        }
+
+        function goUp(){
+            pos=section.getBoundingClientRect();
+            yIndex=pos.top;
+            if(yIndex>=0){
+                clearInterval( IdForSmoothScroll);
+                prevEventId.pop();
+                // endCall++;
+                // console.log("endCall= "+endCall);
+                return ;
+            }
+            window.scrollBy(0,-20);
+            // count++;
+            // console.log("Scroll call = "+count);
+        }
+
+        
     }
 
-    
-}
 
-
-for(let i=0;i<arr.length;i++){
-    arr[i].addEventListener('click',bringTo);
-}
+    for(let i=0;i<arr.length;i++){
+        arr[i].addEventListener('click',bringTo);
+    }
+})();
